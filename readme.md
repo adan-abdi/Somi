e6cb2460f6f12085b43aa85c0028989d3b7b4c36
Abdi-Adan <53440762+Abdi-Adan@users.noreply.github.com>
2020-04-16 17:48:43 +0300

    setup

---
+++ readme.md
@@ -0,0 +1,1 @@
+# Somi - A Digital Interactive Online School For The Next Generation Student 
---
+++ src/somi/settings/production.py
@@ -0,0 +1,37 @@
+from .base import *
+from decouple import config
+
+
+DEBUG = config('DEBUG', cast=bool)
+
+ALLOWED_HOSTS = ['ip-address-server', 'www.your-website.com']
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USERNAME'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
+
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
+]
+
+
+# PAYMENT OPTION LIVE SECRET KEY, LIVE PUBLIC KEY
+# add to .env to secure keys
