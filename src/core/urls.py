365e2a7101e6b7120d3aebae47a9cd4469bd5f98
Abdi-Adan <53440762+Abdi-Adan@users.noreply.github.com>
2020-04-20 01:25:46 +0300

    login, logout auth fix

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.urls import path
 
-from core.views import index
+from core.views import home
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', home, name='home'),
 ]
