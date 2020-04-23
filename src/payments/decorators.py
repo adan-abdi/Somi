95e2303d79eaede54f07676549b0e816ceffcec9
allansifuna <allansifuna324@gmail.com>
2020-04-23 21:12:49 +0300

    Update on Payments

---
+++ src/payments/decorators.py
@@ -0,0 +1,7 @@
+#@subscription_required decorator algorithm
+#from datetime import datetime
+#today=datetime.today()
+#use subscription=request.user.subscription_set.filter(start_timestamp<=today,end_timestamp>=today).all()
+# if len(subscription)==0
+#redirect them to the subscription url with an alert to explain to the user
+#else just pass
