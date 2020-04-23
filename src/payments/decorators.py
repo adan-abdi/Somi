5a4b9bb910f4d61beae8b0a818392014e149c834
allansifuna <allansifuna324@gmail.com>
2020-04-23 21:28:30 +0300

    Update on Payments

--- src/payments/decorators.py
+++ src/payments/decorators.py
@@ -1,7 +1,22 @@
 #@subscription_required decorator algorithm
 #from datetime import datetime
-#today=datetime.today()
-#use subscription=request.user.subscription_set.filter(start_timestamp<=today,end_timestamp>=today).all()
+# today=datetime.today()
+# use
 # if len(subscription)==0
-#redirect them to the subscription url with an alert to explain to the user
-#else just pass
+# redirect them to the subscription url with an alert to explain to the user
+# else just pass
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import redirect
+
+
+def subscription_required(function):
+    def wrap(request, *args, **kwargs):
+        subscription = request.user.subscription_set.filter(
+            start_timestamp <= today, end_timestamp >= today).all()
+        if len(subscription) == 0:
+            return function(request, *args, **kwargs)
+        else:
+            return redirect('/success/')
+    wrap.__doc__ = function.__doc__
+    wrap.__name__ = function.__name__
+    return wrap
