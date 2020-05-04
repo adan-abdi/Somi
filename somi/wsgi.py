eae0cb3baac2cf3f7e6e4d255677b5a5aa9c1a9a
Abdi-Adan <53440762+Abdi-Adan@users.noreply.github.com>
2020-05-05 00:42:40 +0300

    restructuring git index

--- somi/wsgi.py
+++ somi/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for Scribnotes project.
+WSGI config for halisi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Scribnotes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'somi.settings.development')
 
 application = get_wsgi_application()
