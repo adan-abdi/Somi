b3ce6615b5054917bb140608954a8d395663e19e
Abdi-Adan <53440762+Abdi-Adan@users.noreply.github.com>
2020-05-09 22:15:45 +0300

    recreating frontend

--- core/views.py
+++ core/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
-
-# def home(request):
-#     return render(request, 'base.html', {})
+    return render(request, 'homepage/main.html', {'n' : range(3) })
